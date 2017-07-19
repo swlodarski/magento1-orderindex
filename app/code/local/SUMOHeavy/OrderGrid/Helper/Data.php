dc161c922116a13eb43f3762179871e019d7e413
Daniel Szubart <d.szubart@virtuasoftware.com>
2017-07-19 17:28:20 +0200

    SHL-5 Magento1 indexer order grid

---
+++ app/code/local/SUMOHeavy/OrderGrid/Helper/Data.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * SUMOHeavy_OrderGrid
+ *
+ * @category   SUMOHeavy
+ * @package    SUMOHeavy_OrderGrid
+ * @copyright  Copyright (c) 2017 SUMO Heavy Industries, LLC
+ * @author     Szymon Wlodarski <support@sumoheavy.com>
+ * @author     Daniel Szubart <support@sumoheavy.com>
+ */
+class SUMOHeavy_OrderGrid_Helper_Data extends Mage_Core_Helper_Abstract
+{
+    const XML_CONFIG_ORDERGRID_ENABLED = 'sumoheavy_ordergrid/general/enabled';
+
+    public function isEnabled($store = null)
+    {
+        return Mage::getStoreConfigFlag(self::XML_CONFIG_ORDERGRID_ENABLED, $store);
+    }
+
+    protected $_logFileName = 'sumoheavy_ordergrid.log';
+
+    public function updateSalesFlatOrderGrid()
+    {
+        if(!$this->isEnabled()) {
+            return $this;
+        }
+
+        $this->_log('Starting updateSalesFlatOrderGrid');
+
+        $dateThreshold = Mage::getSingleton('core/date')
+            ->gmtDate(null, Mage::getSingleton('core/date')->date(null, '-25 minutes'));
+
+        $resourceModel = Mage::getResourceModel('sales/order');
+        $subSelect = $resourceModel->getReadConnection()
+            ->select()
+            ->from($resourceModel->getGridTable(), 'entity_id');
+
+        $collection = Mage::getResourceModel('sales/order_collection')
+            ->addFieldToSelect('entity_id')
+            ->addFieldToFilter(array(
+                'updated_at',
+                'entity_id',
+            ),
+                array(
+                    array('gt' => $dateThreshold),
+                    array('nin' => $subSelect),
+                ));
+
+        $orderIds = $resourceModel->getReadConnection()->fetchCol($collection->getSelect());
+
+        if(!empty($orderIds)) {
+            $resourceModel->updateGridRecords($orderIds);
+            $this->_log('Orders with IDs: ' . implode(', ', $orderIds) . ' were updated.');
+        }
+
+        $this->_log('Finished updateSalesFlatOrderGrid');
+
+        return $this;
+    }
+
+    protected function _log($message)
+    {
+        Mage::log($message, null, $this->_logFileName);
+    }
+}
---
+++ app/code/local/SUMOHeavy/OrderGrid/Model/Observer.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * SUMOHeavy_OrderGrid
+ *
+ * @category   SUMOHeavy
+ * @package    SUMOHeavy_OrderGrid
+ * @copyright  Copyright (c) 2017 SUMO Heavy Industries, LLC
+ * @author     Szymon Wlodarski <support@sumoheavy.com>
+ * @author     Daniel Szubart <support@sumoheavy.com>
+ */
+class SUMOHeavy_OrderGrid_Model_Observer
+{
+    public function onSalesOrderSaveAfter(Varien_Event_Observer $observer)
+    {
+        if(!Mage::helper('sumoheavy_ordergrid')->isEnabled()) {
+            return $this;
+        }
+
+        $order = $observer->getEvent()->getOrder();
+        if(!Mage::app()->getStore()->isAdmin()) {
+            $order->setForceUpdateGridRecords(true);
+        }
+
+        $index = Mage::getModel('index/process')
+            ->getCollection()
+            ->addFieldToFilter('indexer_code','sumoheavy_ordergrid')
+            ->getFirstItem();
+
+        if ($index && $index->getId()) {
+            $eventResource = Mage::getResourceSingleton('index/event');
+            $unprocessedEvents = $eventResource->getUnprocessedEvents($index);
+            if (empty($unprocessedEvents)) {
+                Mage::getModel('index/indexer')->logEvent(
+                    $order,
+                    'sumoheavy_ordergrid_index',
+                    Mage_Index_Model_Event::TYPE_SAVE
+                );
+            }
+        }
+        
+        return $this;
+    }
+}
